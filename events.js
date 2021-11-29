c20e7a238a219b402958b2db959a91f7c7458aa6
Manuela <ellaheckenberger@gmail.com>
2021-11-29 14:38:44 +0100

    fertig

--- events.js
+++ events.js
@@ -1,19 +1,52 @@
-
 /********  Variablen **********/
+
+const btn = (document.getElementById("trigBtn"));
+
 
 
 
 /********  Event-Listener **********/
 
-
+window.addEventListener("load", toggleAppStatus);
+btn.addEventListener("click", toggleAppStatus);
+let appStatus = true;
 
 /********  Business-Logic | Toggle **********/
 
+function toggleAppStatus() {
+    appStatus = !appStatus;
+    updateView();
 
+}
 
 /********  Änderung in View-Schicht **********/
+//switchClassName("night")
+//switchClassName("day")
 
-/
+function updateView() {
+    if (appStatus) {
+        switchClassName("night");
+        switchBtnTxt("day");
+    } else {
+        switchClassName("day");
+        switchBtnTxt("night");
+    }
+
+}
+
+function switchClassName(modeStr) {
+    document.body.className = modeStr;
+    document.body.children[0].className = modeStr;
+    document.body.children[1].className = modeStr;
+}
+
+// Modul: Umschaltung BtnTxt | Test:
+//switchBtnTxt("night");
+//switchBtnTxt("day");
+
+function switchBtnTxt(modeStr) {
+    btn.innerHTML = modeStr;
+}
 
 /* Tools */
 function output(outputData) {
